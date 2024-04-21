8ef5eb2f7374968aa56834289e05dc9b34a81cc2
harshuk101 <harshuk101@gmail.com>
2024-04-21 10:37:05 -0400

    starting, god help

---
+++ helpers.py
@@ -0,0 +1,36 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.utils.data as data
+import math
+import copy
+
+
+# https://arxiv.org/pdf/1706.03762v7.pdf
+class Transformer(nn.Module):
+    def __init__(self,  num_encoder_layers:int = 6):
+        super().__init__()
+
+        self.FFLayers = 
+
+
+class Encoder(nn.Module):
+
+
+
+
+class MultiHeadAttention(nn.Module):
+
+    #Input: T vectors of size M => T x M where each vector is a word
+    def __init__(self, len_embedding = 8, num_heads = 4):
+        super().__init__()
+        self.len_embedding, self.num_heads = len_embedding, num_heads
+        self.q = nn.Linear(len_embedding, len_embedding, biase=False)
+        self.k = nn.Linear(len_embedding, len_embedding, biase=False)
+        self.v = nn.Linear(len_embedding, len_embedding, biase=False)
+
+    def forward(self, queries, key, values):
+        queries = self.
+
+
+    def attention(self, input):
